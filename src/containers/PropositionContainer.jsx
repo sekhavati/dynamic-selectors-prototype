318ea34630833761f3e08617ca874a63a7248a12
Amir Sekhavati <amir.sekhavati@sky.uk>
2018-01-23 17:31:05 +0000

    Add container

---
+++ src/containers/PropositionContainer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const PropositionContainer = ({}) => {
+
+};
+
+const mapStateToProps = (state, ownProps) => {
+
+};
+
+const mapDispatchToProps = dispatch => {
+
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(PropositionContainer);
